81713fda8046b46651a7781a4a77f29776b38103
badwin <jfdream1992@gmail.com>
2024-01-15 11:18:07 +0800

    add support for wss

--- IMWebSocket/DMNetworkStreamer.m
+++ IMWebSocket/DMNetworkStreamer.m
@@ -35,14 +35,15 @@
             port = "80";
         }
         nw_endpoint_t endpoint = nw_endpoint_create_host(hostname, port);
-        
-        nw_parameters_configure_protocol_block_t configure_tls = ^(nw_protocol_options_t  _Nonnull options) {
-            sec_protocol_options_t option = nw_tls_copy_sec_protocol_options(options);
-            sec_protocol_options_set_verify_block(option, ^(sec_protocol_metadata_t  _Nonnull metadata, sec_trust_t  _Nonnull trust_ref, sec_protocol_verify_complete_t  _Nonnull complete) {
-                complete(true);
-            }, _workQueue);
-        };
-        
+        nw_parameters_configure_protocol_block_t configure_tls = NW_PARAMETERS_DISABLE_PROTOCOL;
+        if ([request.URL.scheme isEqualToString:@"wss"]) {
+            configure_tls = ^(nw_protocol_options_t  _Nonnull options) {
+                sec_protocol_options_t option = nw_tls_copy_sec_protocol_options(options);
+                sec_protocol_options_set_verify_block(option, ^(sec_protocol_metadata_t  _Nonnull metadata, sec_trust_t  _Nonnull trust_ref, sec_protocol_verify_complete_t  _Nonnull complete) {
+                    complete(true);
+                }, _workQueue);
+            };
+        }
         nw_parameters_t parameters = nw_parameters_create_secure_tcp(configure_tls,
                                         NW_PARAMETERS_DEFAULT_CONFIGURATION);
         
